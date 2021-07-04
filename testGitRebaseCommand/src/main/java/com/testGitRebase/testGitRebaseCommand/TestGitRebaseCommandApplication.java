66a72ad1712f83393f9bd2f7e54324d69911f4e2
Jadhav, Akshay (aj8557) <aj8557@att.com>
2021-07-04 08:25:45 +0530

    innitial commit

---
+++ testGitRebaseCommand/src/main/java/com/testGitRebase/testGitRebaseCommand/TestGitRebaseCommandApplication.java
@@ -0,0 +1,13 @@
+package com.testGitRebase.testGitRebaseCommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitRebaseCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitRebaseCommandApplication.class, args);
+	}
+
+}
---
+++ testGitRebaseCommand/src/test/java/com/testGitRebase/testGitRebaseCommand/TestGitRebaseCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testGitRebase.testGitRebaseCommand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitRebaseCommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
